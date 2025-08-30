00a3e17075340d463b11d30368577ae32c82cf5d
Neil Lawrence <lawrennd@gmail.com>
2025-08-30 08:37:25 +0100

    Remove outdated test files and setup.py, add modern notebook template

--- access_tests.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import nose, warnings
-
-# THese tests should be checking the known access problems, such as whether an excel file has turned a gene to a date or has more than 1,048,576 rows.
-nose.main("fynesse", defaultTest="fynesse/tests/access", argv=["", ""])
--- assess_tests.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-import nose, warnings
-
-nose.main("fynesse", defaultTest="fynesse/tests/assess", argv=["", ""])
